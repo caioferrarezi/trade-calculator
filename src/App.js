35c14d27b6ebafa35c0a5e23092c07c7049cc212
Caio Ferrarezi <caioferrarezi2@gmail.com>
2019-05-31 22:13:13 -0300

    quick file structure reorganization

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
 import React, { Component } from 'react'
 import './App.css'
-import Hero from '../src/hero/index'
+import TradeForm from './components/TradeForm'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      trades: []
+    }
+
+    this.bind()
+  }
+
+  bind() {
+    this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    this.hasTrades = this.hasTrades.bind(this)
+    this.hasSpecicTrade = this.hasSpecicTrade.bind(this)
+  }
+
+  handleFormSubmit(trade) {
+    let trades = this.state.trades
+    let tradeIndex = this.hasSpecicTrade(trade.name)
+
+    if (!this.hasTrades() || tradeIndex === -1) {
+      trades.push({
+        name: trade.name,
+        values: [trade.values]
+      })
+    } else {
+      trades[tradeIndex].values.push(trade.values)
+    }
+
+    this.setState({
+      trades: trades
+    })
+
+    console.log(this.state.trades)
+  }
+
+  hasTrades() {
+    return !!this.state.trades.length
+  }
+
+  hasSpecicTrade(name) {
+    return this.state.trades.findIndex(trade => trade.name === name)
+  }
+
   render() {
     return (
       <div className="App">
-        <Hero></Hero>
+        <div className="c-hero">
+          <h1 className="c-hero__title">Trade Calculator</h1>
+
+          <TradeForm onFormSubmit={this.handleFormSubmit}></TradeForm>
+        </div>
       </div>
     )
   }
