84de3d98fca09358a4906ae3786fa373052940e7
Caio Ferrarezi <caioferrarezi2@gmail.com>
2019-06-02 22:06:11 -0300

    add rich form validation

--- src/components/TradeForm.js
+++ src/components/TradeForm.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
 import './TradeForm.css'
+import classnames from 'classnames'
+
+const validators = {
+  isNotBlank: /^(?!\s*$).+/g,
+  isNumber: /^(\d*\.?\d*)$/g
+}
 
 function generateId() {
   return Math.random().toString(36).substring(2, 15) +
@@ -11,12 +17,29 @@ class TradeForm extends Component {
     super(props)
 
     this.state = {
-      type: 'purchase',
-      name: '',
-      price: '',
-      amount: '',
-      fee: '',
-      isFormValid: false
+      inputs: {
+        type: {
+          value: 'purchase',
+          isValid: true,
+        },
+        name: {
+          value: '',
+          isValid: true,
+        },
+        price: {
+          value: 0,
+          isValid: true,
+        },
+        amount: {
+          value: 0,
+          isValid: true,
+        },
+        fee: {
+          value: 0,
+          isValid: true,
+        }
+      },
+      isFormValid: false,
     }
 
     this.inputName = React.createRef()
@@ -27,6 +50,7 @@ class TradeForm extends Component {
   bind() {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.validateInput = this.validateInput.bind(this);
     this.validate = this.validate.bind(this);
     this.reset = this.reset.bind(this);
   }
@@ -37,6 +61,7 @@ class TradeForm extends Component {
 
   componentDidUpdate(prevProp, prevState) {
     let isFormValid = this.validate()
+
     if (prevState.isFormValid !== isFormValid)
       this.setState({
         isFormValid
@@ -68,21 +93,34 @@ class TradeForm extends Component {
 
   handleInputChange(event) {
     const target = event.target
+    let inputs = {...this.state.inputs}
+
+    inputs[target.id].value = target.value.replace(',', '.')
+    inputs[target.id].isValid = this.validateInput(target)
 
     this.setState({
-      [target.id]: target.value.replace(',', '.')
+      inputs
     })
   }
 
-  validate() {
-    let state = this.state;
+  validateInput(input) {
+    const validations = input.dataset.validation.split(', ')
+    let isValid = true;
 
-    let isValid =
-      !!state.type &&
-      !!state.name &&
-      !!state.price &&
-      !!state.amount &&
-      !!state.fee
+    validations.forEach(validation => {
+      isValid = isValid && !!input.value.match(validators[validation])
+    })
+
+    return isValid
+  }
+
+  validate() {
+    const inputs = this.state.inputs
+    let isValid = true;
+
+    Object.keys(inputs).forEach(key => {
+      isValid = isValid && inputs[key].isValid
+    })
 
     return isValid
   }
@@ -98,23 +136,36 @@ class TradeForm extends Component {
   }
 
   render() {
+    const inputs = this.state.inputs
+
     return(
       <form className="c-trade-form" onSubmit={this.handleSubmit}>
         <div className="c-trade-form__wrapper">
           <div className="c-trade-form__field">
             <label className="c-trade-form__label" htmlFor="name">Name:</label>
-            <input className="c-trade-form__input -name"
+            <input className={classnames({
+              'c-trade-form__input -name': true,
+              'is-invalid': !inputs.name.isValid
+            })}
               type="text"
               id="name"
+              data-validation="isNotBlank"
               ref={this.inputName}
-              value={this.state.name}
+              value={inputs.name.value}
               onChange={this.handleInputChange} />
           </div>
 
           <div className="c-trade-form__field">
             <label className="c-trade-form__label" htmlFor="type">Type:</label>
             <div className="c-trade-form__select-wrapper">
-              <select className="c-trade-form__select" id="type" value={this.state.type} onChange={this.handleInputChange}>
+              <select className={classnames({
+                'c-trade-form__select': true,
+                'is-invalid': !inputs.type.isValid
+              })}
+                id="type"
+                data-validation="isNotBlank"
+                value={inputs.type.value}
+                onChange={this.handleInputChange}>
                 <option value="purchase">Purchase</option>
                 <option value="sale">Sale</option>
               </select>
@@ -123,28 +174,40 @@ class TradeForm extends Component {
 
           <div className="c-trade-form__field">
             <label className="c-trade-form__label" htmlFor="price">Price:</label>
-            <input className="c-trade-form__input"
+            <input className={classnames({
+              'c-trade-form__input': true,
+              'is-invalid': !inputs.price.isValid
+            })}
               type="text"
               id="price"
-              value={this.state.price}
+              data-validation="isNotBlank, isNumber"
+              value={inputs.price.value}
               onChange={this.handleInputChange} />
           </div>
 
           <div className="c-trade-form__field">
             <label className="c-trade-form__label" htmlFor="amount">Amount:</label>
-            <input className="c-trade-form__input"
+            <input className={classnames({
+              'c-trade-form__input': true,
+              'is-invalid': !inputs.amount.isValid
+            })}
               type="text"
               id="amount"
-              value={this.state.amount}
+              data-validation="isNotBlank, isNumber"
+              value={inputs.amount.value}
               onChange={this.handleInputChange} />
           </div>
 
           <div className="c-trade-form__field">
             <label className="c-trade-form__label" htmlFor="fee">Fee:</label>
-            <input className="c-trade-form__input"
+            <input className={classnames({
+              'c-trade-form__input': true,
+              'is-invalid': !inputs.fee.isValid
+            })}
               type="text"
               id="fee"
-              value={this.state.fee}
+              data-validation="isNotBlank, isNumber"
+              value={inputs.fee.value}
               onChange={this.handleInputChange} />
           </div>
 
